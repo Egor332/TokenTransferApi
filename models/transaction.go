2d4036dcf98606ffc1f70dcff37675fbae93d404
Egor Kachanouski <kegor2323@gmail.com>
2025-11-13 18:45:17 +0100

    Refactored

--- models/transaction.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Transaction struct {
-	ID          int64  `gorm:"column:id;PrimaryKey"`
-	FromAddress string `gorm:"column:from_address;type:varchar(42);not null"`
-	ToAddress   string `gorm:"column:to_address;type:varchar(42);not null"`
-	Amount      int64  `gorm:"column:amount;not null"`
-}
